122aa038d1e9d126301da0cc90d15275c82e2a2d
am22ar <amarpatil12222@gmail.com>
2022-09-30 20:06:05 +0530

    Added: Logic to solve Line Comparison Problem using OOPS concept

---
+++ src/com/oops/linecomparison/LineComparisonMain.java
@@ -0,0 +1,13 @@
+package com.oops.linecomparison;
+
+public class LineComparisonMain {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Program using OOPS Concept");
+		LineComparison lc = new LineComparison();
+		lc.getLength();
+		lc.compareLines();
+
+	}
+
+}
